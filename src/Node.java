a7b245475b38417bf034adf297dfa55632a47049
MHEN2606 <matthew.mahendra@gmail.com>
2023-04-06 13:45:12 +0700

    Added Node

---
+++ src/Node.java
@@ -0,0 +1,50 @@
+public class Node implements Comparable<Node>{
+    private String current;
+    private String goal;
+    private int gn;
+    private int hn;
+    private String[] visited;
+
+    public Node(String c, String g, int gn, int hn, String[] visited){
+        current=c;
+        goal = g;
+        this.gn = gn;
+        this.hn = hn;
+        this.visited = visited;
+    }
+
+    public String getCurrent(){
+        return current;
+    }
+
+    public String getGoal(){
+        return goal;
+    }
+
+    public int getGn(){
+        return gn;
+    }
+
+    public int getHn(){
+        return hn;
+    }
+
+    public int calculateFN(){
+        return hn+gn;
+    }
+
+    public String getPath(){
+        return visited.toString();
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if(calculateFN() < o.calculateFN()){
+            return -1;
+        }else if (calculateFN() < o.calculateFN()){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+}
