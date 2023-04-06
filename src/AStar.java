a312685fcfd1c27e944e0c953f47cf988fe3b19a
MHEN2606 <matthew.mahendra@gmail.com>
2023-04-07 02:17:17 +0700

    feat: Bisa baca kalo gak ada koordinatnya

--- src/AStar.java
+++ src/AStar.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class AStar{
     private PriorityQueue<Node> queue;
     private Graph graph;
+    private double jarak = 0;
 
     public AStar(String startPoint, String endPoint, String filename){
         graph = new Graph(filename);
@@ -18,39 +19,49 @@ public class AStar{
             Node check = queue.remove();
 
             if(check.getCurrent().equals(check.getGoal())){
+                jarak = check.calculateFN();
                 return (check.getPath());
             }
 
             for(int i = 0; i < graph.getNodes() ; i++){
                 if(graph.getGraph(graph.getIndex(check.getCurrent()), i) > 0 && !check.getPath().contains(graph.getLocName(i))){
                     /* Masukkan ke prioqueue 
-                     * Buat nodes baru
-                     */
+                    * Buat nodes baru
+                    */
                     ArrayList<String> visitNew = new ArrayList<>(check.getPath());
 
                     visitNew.add(graph.getLocName(i));
-
-                    Node newNode = new Node(graph.getLocName(i), 
+                    Node newNode;
+                    /* Kondisi Bonus */
+                    if(graph.getPos(graph.getLocName(0))[0] != -9999){
+                        newNode = new Node(graph.getLocName(i), 
                                             check.getGoal(), 
                                             graph.getGraph(graph.getIndex(check.getCurrent()), i) + check.getGn(), 
                                             graph.euclideanDistance(graph.getPos(graph.getLocName(i)), graph.getPos(check.getGoal())),
                                             visitNew);
-
+                    }else{
+                    /* Kondisi Bukan Bonus */
+                        newNode = new Node(graph.getLocName(i), 
+                                            check.getGoal(), 
+                                            graph.getGraph(graph.getIndex(check.getCurrent()), i) + check.getGn(), 
+                                            Math.abs(graph.getIndex(graph.getLocName(i))) - graph.getIndex(check.getGoal()),
+                                            visitNew);
+                    }
                     queue.add(newNode);
                 }
             }
-
         }
         return new ArrayList<>();
     }
 
     public static void main(String[] args){
-        AStar a = new AStar("Merdeka_Timur", "M.H._Thamrin", "map1.txt");
+        AStar a = new AStar("1", "2", "map1.txt");
         ArrayList<String> path = a.Solver();
 
         for(int i = 0; i < path.size(); i++){
             System.out.println(path.get(i));
         }
+        System.out.println(a.jarak);
     }
 
 }
